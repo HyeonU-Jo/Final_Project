6218636a4b45f706490d292a6aeb09fcfd8e653f
Zinisang <86811543+Zinisang@users.noreply.github.com>
2021-07-16 14:06:23 +0900

    Update MemberService.java

--- src/main/java/com/work/finalproject/service/MemberService.java
+++ src/main/java/com/work/finalproject/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.work.finalproject.service;
 
+import com.work.finalproject.dto.MemberDTO;
+import com.work.finalproject.entity.member_tbl;
+
 public interface MemberService {
 
     //로그인
@@ -9,4 +12,25 @@ public interface MemberService {
     //마이페이지
 
 
+    // DTO -> Entity
+    default member_tbl dtoToEntity(MemberDTO dto) {
+        member_tbl entity = member_tbl.builder()
+                .id(dto.getId())
+                .password(dto.getPassword())
+                .email(dto.getEmail())
+                .name(dto.getName())
+                .build();
+        return entity;
+    }
+
+    // Entity -> DTO
+    default MemberDTO entityToDto(member_tbl entity){
+        MemberDTO dto = MemberDTO.builder()
+                .id(entity.getId())
+                .password(entity.getPassword())
+                .email(entity.getEmail())
+                .name(entity.getName())
+                .build();
+        return dto;
+    }
 }
