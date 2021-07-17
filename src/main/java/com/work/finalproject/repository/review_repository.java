9bda540c4a3b14060cfd49f350096cc864219463
Geun <kendong18@gmail.com>
2021-07-17 15:23:42 +0900

    리스트 불러오기 아직 하는 중
    123

--- src/main/java/com/work/finalproject/repository/review_repository.java
+++ src/main/java/com/work/finalproject/repository/review_repository.java
@@ -1,7 +1,12 @@
 package com.work.finalproject.repository;
 
+import com.work.finalproject.dto.ReviewDTO;
 import com.work.finalproject.entity.review_tbl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface review_repository extends JpaRepository<review_tbl, String> {
+//    List<ReviewDTO> findByContent_id(String content_id);
+    List<ReviewDTO> findByContent_idContains(String content_id);
 }
---
+++ src/test/java/com/work/finalproject/repository/ReviewRepository.java
@@ -0,0 +1,17 @@
+package com.work.finalproject.repository;
+
+import com.work.finalproject.entity.Qreview_tbl;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ReviewRepository {
+
+    @Test
+    public void selectTest(){
+        Qreview_tbl t = Qreview_tbl.review_tbl;
+
+
+    }
+
+}
