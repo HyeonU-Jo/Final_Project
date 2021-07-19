c022465600dd14ca2a9fc4a99df87524261ed5f2
Geun <kendong18@gmail.com>
2021-07-19 17:27:07 +0900

    JPA 쿼리 작성 성공
    쿼리dsl은 다음에 사용

--- src/main/java/com/work/finalproject/repository/review_repository.java
+++ src/main/java/com/work/finalproject/repository/review_repository.java
@@ -7,8 +7,10 @@ import com.work.finalproject.entity.review_tbl;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+import java.util.List;
+
 
 public interface review_repository extends JpaRepository<review_tbl, String>, QuerydslPredicateExecutor<review_tbl> {
     @Query("select r from review_tbl r where r.content_id = ?1")
-    review_tbl findByContent_id(String content_id);
+    List<review_tbl> findByContent_id(String content_id);
 }
--- src/test/java/com/work/finalproject/repository/ReviewRepository.java
+++ src/test/java/com/work/finalproject/repository/ReviewRepository.java
@@ -2,15 +2,38 @@ package com.work.finalproject.repository;
 
 import com.work.finalproject.entity.Qreview_tbl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.work.finalproject.entity.review_tbl;
+
+import java.util.List;
 
 @SpringBootTest
 public class ReviewRepository {
+
+    @Autowired
+    private review_repository repository;
 
     @Test
     public void selectTest(){
         Qreview_tbl t = Qreview_tbl.review_tbl;
 
+
+    }
+    @Test
+    public void findByContent_id(){
+        review_tbl revt = new review_tbl();
+
+
+        revt.setContent_id("2458633");
+
+
+
+        List<review_tbl> all = repository.findByContent_id("2458633");
+
+        for (int i = 0; i<all.size(); i++){
+            System.out.println("값 확인한다. "+all.get(i).getR_content());
+        }
 
     }
 
