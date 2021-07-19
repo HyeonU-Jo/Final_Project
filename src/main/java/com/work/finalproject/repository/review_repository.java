e2eb08b33133b0cf20b9a6868d6950bcfac95142
Geun <kendong18@gmail.com>
2021-07-19 16:47:48 +0900

    Update review_repository.java
    쿼리문 테스트

--- src/main/java/com/work/finalproject/repository/review_repository.java
+++ src/main/java/com/work/finalproject/repository/review_repository.java
@@ -4,8 +4,11 @@ package com.work.finalproject.repository;
 import com.work.finalproject.entity.review_tbl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.work.finalproject.entity.review_tbl;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 
 public interface review_repository extends JpaRepository<review_tbl, String>, QuerydslPredicateExecutor<review_tbl> {
+    @Query("select r from review_tbl r where r.content_id = ?1")
+    review_tbl findByContent_id(String content_id);
 }
