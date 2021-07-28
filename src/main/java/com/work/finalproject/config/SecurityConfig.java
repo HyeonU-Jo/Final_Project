925a6a00fd9363cc12c6be8d5baedee0710784f4
forsomeday97 <forsomeday97@naver.com>
2021-07-28 14:29:42 +0900

    로그인 여부에 따른 header변화
    속이뻥

--- src/main/java/com/work/finalproject/config/SecurityConfig.java
+++ src/main/java/com/work/finalproject/config/SecurityConfig.java
@@ -47,7 +47,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .loginPage("/member/auth/login")
                 .loginProcessingUrl("/auth/loginProc") //스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채서 대신 로그인
                 .defaultSuccessUrl("/") //로 이동
-                .failureUrl("/fail");
+                .failureUrl("/fail")
+            .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/");
 
     }
 }
