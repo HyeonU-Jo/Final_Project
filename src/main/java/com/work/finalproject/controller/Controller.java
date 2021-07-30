db5c6ce7a3bfd7b9c9ccc1f21b79dca3aaf14a29
HyeonU-Jo <chw3448@gmail.com>
2021-07-30 15:21:17 +0900

    CSS 중간 정리

--- src/main/java/com/work/finalproject/controller/Controller.java
+++ src/main/java/com/work/finalproject/controller/Controller.java
@@ -2,15 +2,14 @@ package com.work.finalproject.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
 @Log4j2
 public class Controller {
 
     @GetMapping({"/"})
-    public String test() {
+    public String index() {
 
-        return "test";
+        return "index";
     }
 }
--- src/main/java/com/work/finalproject/controller/MemberController.java
+++ src/main/java/com/work/finalproject/controller/MemberController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/member")
@@ -44,7 +43,7 @@ public class MemberController {
     @PostMapping("/login")
     public String loginPost(){
 
-        return "/main";
+        return "index";
     }
 
 
