8a4214cb6eac99d38dc83ccf9e1eb749aab75c52
jangboraa <wkdqhfk92@naver.com>
2021-08-11 14:14:13 +0900

    필요없는 파일 정리

--- src/main/java/com/work/finalproject/repository/shop_repository.java
+++
@@ -1,8 +0,0 @@
-package com.work.finalproject.repository;
-
-import com.work.finalproject.entity.shop_tbl;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface shop_repository extends JpaRepository<shop_tbl, Integer> {
-
-}
