0f1d3f1b6e518d03473e313a568653e434e448dd
forsomeday97 <forsomeday97@naver.com>
2021-08-13 15:05:48 +0900

    alert

--- src/main/java/com/work/finalproject/config/SecurityConfig.java
+++ src/main/java/com/work/finalproject/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .and()
                 .logout()
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/");
+                .logoutSuccessUrl("/logoutPage");
 
     }
 
